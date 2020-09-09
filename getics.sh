da40e6e9586186255cb6730256b119557e9b731a
Daniel Niemczok <biuro@tmask.pl>
2020-09-09 14:29:34 +0200

    Update getics.sh

--- getics.sh
+++ getics.sh
@@ -1,1 +1,1 @@
-wget --no-check-certificate --output-document="/home/pi/MagicMirror/kalendarz.ics" "https://nc.tmask.pl/remote.php/dav/public-calendars/LSsM8HX35aeY9a8S?export"
+wget --no-check-certificate --output-document="/home/pi/MagicMirror/kalendarz.ics" "https://ip/remote.php/dav/public-calendars/LSsM8HX35aeY9a8S?export"
